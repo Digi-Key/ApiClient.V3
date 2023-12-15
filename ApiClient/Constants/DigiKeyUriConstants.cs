ae26e4083931da5e448b3efb1cf75b3539f7ded2
JackMBurch <jackb@worthingtonassembly.com>
2023-12-15 15:57:56 -0500

    Updating constants to use environment variables for production vs sandbox

--- ApiClient/Constants/DigiKeyUriConstants.cs
+++ ApiClient/Constants/DigiKeyUriConstants.cs
@@ -18,19 +18,41 @@ namespace ApiClient.Constants
     /// </summary>
     public static class DigiKeyUriConstants
     {
-        // Production Sandbox instance
-        //public static readonly Uri BaseAddress = new Uri("https://sandbox-api.digikey.com");
-        //public static readonly Uri TokenEndpoint = new Uri("https://sandbox-api.digikey.com/v1/oauth2/token");
-        //public static readonly Uri AuthorizationEndpoint = new Uri("https://sandbox-api.digikey.com/v1/oauth2/authorize");
+        public static bool GetProductionVariable()
+        {
+            try
+            {
+                return bool.Parse(Environment.GetEnvironmentVariable("DIGIKEY_PRODUCTION")!);
+            }
+            catch
+            {
+                throw new System.Exception("Issue getting the DIGIKEY_PRODUCTION environment variable. Make sure the variable is set to either true or false.");
+            }
+        }
 
-        // Integration instance
-        // public static readonly Uri BaseAddress = new Uri("https://apiint.digikey.com");
-        // public static readonly Uri TokenEndpoint = new Uri("https://apiint.digikey.com/v1/oauth2/token");
-        // public static readonly Uri AuthorizationEndpoint = new Uri("https://apiint.digikey.com/v1/oauth2/authorize");
+        public static Uri BaseAddress
+        {
+            get { return GetProductionVariable() ? ProductionBaseAddress : SandboxBaseAddress; }
+        }
+
+        public static Uri TokenEndpoint
+        {
+            get { return GetProductionVariable() ? ProductionTokenEndpoint : SandboxTokenEndpoint; }
+        }
+
+        public static Uri AuthorizationEndpoint
+        {
+            get { return GetProductionVariable() ? ProductionAuthorizationEndpoint : SandboxAuthorizationEndpoint; }
+        }
+
+        // Production Sandbox instance
+        public static readonly Uri SandboxBaseAddress = new("https://sandbox-api.digikey.com");
+        public static readonly Uri SandboxTokenEndpoint = new("https://sandbox-api.digikey.com/v1/oauth2/token");
+        public static readonly Uri SandboxAuthorizationEndpoint = new("https://sandbox-api.digikey.com/v1/oauth2/authorize");
 
         // Production instance
-        public static readonly Uri BaseAddress = new("https://api.digikey.com");
-        public static readonly Uri TokenEndpoint = new("https://api.digikey.com/v1/oauth2/token");
-        public static readonly Uri AuthorizationEndpoint = new("https://api.digikey.com/v1/oauth2/authorize");
+        public static readonly Uri ProductionBaseAddress = new("https://api.digikey.com");
+        public static readonly Uri ProductionTokenEndpoint = new("https://api.digikey.com/v1/oauth2/token");
+        public static readonly Uri ProductionAuthorizationEndpoint = new("https://api.digikey.com/v1/oauth2/authorize");
     }
 }
